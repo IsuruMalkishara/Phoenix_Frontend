d38c4cee3137456271330d8314c759c5d97f31bc
Malkishara <isurumalkishara97@gmail.com>
2023-05-22 18:05:39 +0530

    display all vacancies in home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,11 @@ import { useParams,useNavigate } from 'react-router-dom';
 import '../styles/HomePage.css';
 import NavBarComponent from './NavBarComponent';
 import { Card, Form, Button} from 'react-bootstrap';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
 import JobCategoryService from '../services/JobCategoryService';
 import JobTypeService from '../services/JobTypeService';
 import JobModalityService from '../services/JobModalityService';
+import VacancyService from '../services/VacancyService';
 
 function HomePage() {
 
@@ -14,14 +16,17 @@ function HomePage() {
     const [categoryList, setCategoryList] = useState([]);
     const [categoryId,setCategoryId]=useState('');
     const [selectedTypes, setSelectedTypes] = useState([]);
-  const [selectedModalities, setSelectedModalities] = useState([]);
+    const [selectedModalities, setSelectedModalities] = useState([]);
+    const [vacancies, setVacancies]=useState([]);
+    const [expandedVacancyId, setExpandedVacancyId] = useState(null);
     
 
     useEffect(() => {
 
         getType();
         getCategory();
-        getModality() }, []);
+        getModality();
+      getAllVacancy();}, []);
     
    
 
@@ -77,6 +82,23 @@ const getModality=()=>{
       );
     }
   };
+
+  //get Vacancy list
+const getAllVacancy=()=>{
+    VacancyService.getAllVacancy().then(res=>{
+    console.warn(res.data);
+    setVacancies(res.data);
+    })
+    }
+
+    // Function to handle vacancy title click and toggle visibility
+const handleVacancyClick = (vacancyId) => {
+  if (expandedVacancyId === vacancyId) {
+    setExpandedVacancyId(null);
+  } else {
+    setExpandedVacancyId(vacancyId);
+  }
+};
 
   return (
     <div className='home'>
@@ -144,7 +166,68 @@ const getModality=()=>{
                  </Card.Body>
             </Card>
             </div>
-            <div className='col-9'></div>
+            <div className='col-9'>
+              {
+                vacancies.map((vacancy)=>(
+                  <Card style={{ margin:'20px' , background:'rgba(255, 255, 255, 0.137)'}}>
+                 <Card.Body>
+
+                  <div className='row'><div className='col' style={{ textAlign:'right' }}><Button variant="primary">Apply</Button></div></div>
+                   <div className='row'>
+                    <div className='col-2'>
+                    <img
+              src={vacancy.employer.logo}
+              width="70"
+              height="70"
+              className="d-inline-block align-top"
+              alt="Phoenix Logo"
+            />
+                    </div>
+                    <div className='col-10' style={{ textAlign:'left' }}>
+                    <div
+                  className='row'
+                 onClick={() => handleVacancyClick(vacancy.id)}
+                 style={{ cursor: 'pointer' }}><h5>{vacancy.title}</h5></div>
+                      <div className='row'><h6>
+                        {vacancy.employer.verification &&(
+                        <VerifiedUserIcon/> )}{vacancy.employer.name}</h6></div>
+                      <div className='row'><p>{vacancy.employer.address}</p></div>
+                    </div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-4'style={{textAlign:'left'}}><p>Category: {vacancy.category.title}</p></div>
+                      <div className='col-4'style={{textAlign:'left'}}><p>Type: {vacancy.type.title}</p></div>
+                      <div className='col-4'style={{textAlign:'left'}}><p>Modality: {vacancy.modality.title}</p></div>
+                    </div>
+                    <div className='row'>
+                      <div className='col-6' style={{textAlign:'left'}}><p>Posted Date: {vacancy.postedDate}</p></div>
+                      <div className='col-6' style={{textAlign:'left'}}><p>Expiration Date: {vacancy.expirationDate}</p></div>
+                      
+                    </div>
+                    
+                    <div className='row'>
+                    {expandedVacancyId === vacancy.id && (
+                      <div className='col' style={{textAlign:'left'}}> <div dangerouslySetInnerHTML={{ __html: vacancy.description }} /></div>
+                    )}
+                      </div>
+
+                    <div className='row' style={{ color:'#FFFF',marginTop:'10px',textAlign:'center' }}>
+  {vacancy.descriptionImg && expandedVacancyId === vacancy.id && (
+    <div>
+      <img
+        src={vacancy.descriptionImg}
+        alt='description'
+        height={'350px'}
+        width={'350px'}
+      />
+    </div>
+  )}
+</div>
+                 </Card.Body>
+                 </Card>
+                ))
+              }
+            </div>
         </div>
       </div>
     </div>
